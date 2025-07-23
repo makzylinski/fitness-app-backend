0091b0c4328a1a3995eb1a7ac5568141fdf428cd
Maks <makzylinski@gmail.com>
2025-07-23 12:04:50 +0200

    Add AuthController for user registration endpoint

---
+++ fitnessapp/src/main/java/com/fitnessapp/fitnessapp/controller/AuthController.java
@@ -0,0 +1,24 @@
+package com.fitnessapp.fitnessapp.controller;
+
+import com.fitnessapp.fitnessapp.dto.RegisterRequest;
+import com.fitnessapp.fitnessapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public String register(@RequestBody RegisterRequest registerRequest) {
+        userService.register(registerRequest);
+
+        return "User registered successfully";
+    }
+}
