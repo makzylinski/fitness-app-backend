7fa0effcceac479ad388863bf813b9291f26ef62
Maks <makzylinski@gmail.com>
2025-08-07 16:29:49 +0200

    Added Workout Type Controller and db connection to get the data

--- fitnessapp/src/main/java/com/fitnessapp/fitnessapp/controller/WorkoutTypeController.java
+++ fitnessapp/src/main/java/com/fitnessapp/fitnessapp/controller/WorkoutTypeController.java
@@ -1,17 +1,28 @@
 package com.fitnessapp.fitnessapp.controller;
 
 import com.fitnessapp.fitnessapp.dto.WorkoutTypeResponse;
+import com.fitnessapp.fitnessapp.model.WorkoutType;
+import com.fitnessapp.fitnessapp.repository.WorkoutTypeRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/workout-types")
 public class WorkoutTypeController {
+    private final WorkoutTypeRepository workoutTypeRepository;
+
+    public WorkoutTypeController(WorkoutTypeRepository workoutTypeRepository) {
+        this.workoutTypeRepository = workoutTypeRepository;
+    }
+
     @GetMapping
-    public ResponseEntity<WorkoutTypeResponse> getWorkoutTypes() {
+    public ResponseEntity<List<WorkoutType>> getWorkoutTypes() {
         WorkoutTypeResponse response = new WorkoutTypeResponse();
-        return ResponseEntity.ok(response);
+        List<WorkoutType> workoutTypes = workoutTypeRepository.findAll();
+        return ResponseEntity.ok(workoutTypes);
     }
 }
