83e7e9bba52679387d6dda0791398c1cc220490b
Maks <makzylinski@gmail.com>
2025-10-07 16:43:42 +0200

    Add WorkoutExerciseRepository and WorkoutSetRepository interfaces for managing workout exercises and sets

---
+++ fitnessapp/src/main/java/com/fitnessapp/fitnessapp/repository/WorkoutSetRepository.java
@@ -0,0 +1,6 @@
+package com.fitnessapp.fitnessapp.repository;
+
+import com.fitnessapp.fitnessapp.model.WorkoutSet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkoutSetRepository extends JpaRepository<WorkoutSet, Long> {}
