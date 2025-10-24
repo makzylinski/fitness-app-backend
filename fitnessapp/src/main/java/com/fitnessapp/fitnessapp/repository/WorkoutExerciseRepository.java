7198accd9c68381cfc642e04ae7038dc519c02d5
Maks <makzylinski@gmail.com>
2025-10-24 20:58:01 +0200

    Remove unused files

--- fitnessapp/src/main/java/com/fitnessapp/fitnessapp/repository/WorkoutExerciseRepository.java
+++
@@ -1,6 +0,0 @@
-package com.fitnessapp.fitnessapp.repository;
-
-import com.fitnessapp.fitnessapp.model.WorkoutExercise;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkoutExerciseRepository extends JpaRepository<WorkoutExercise, Long> {}
